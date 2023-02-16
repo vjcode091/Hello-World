03fee55ed3b99fae910a3f82bf44100671af50da
VICTOR <vjcode091@gmail.com>
2023-02-16 08:23:33 -0800

    SECOND COMMIT

---
+++ script.js
@@ -0,0 +1,7 @@
+let a = 20
+let b = 10
+let c = -10
+
+console.log(a == b)
+console.log(a != b)
+alert("welcome to victors website how can we help you. dominion computer school is the best place to study your computer lessons")
